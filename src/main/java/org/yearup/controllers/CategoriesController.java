60c962420078be9e20cbd1288cf94c793c48e87e
Jack Skjong-Nilsen <jacksnilsen@gmail.com>
2025-06-23 15:47:02 -0500

    Add return getAllCategories method and test on Postman/web app

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,6 +1,7 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
@@ -24,11 +25,11 @@ public class CategoriesController
         this.categoryDao = categoryDao;
     }
 
-    // add the appropriate annotation for a get action
+    @GetMapping
     public List<Category> getAll()
     {
         // find and return all categories
-        return null;
+        return categoryDao.getAllCategories();
     }
 
     // add the appropriate annotation for a get action
