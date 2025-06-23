9cd2de3e086374a7d9d135f9b36da7b1f7e913c6
Jack Skjong-Nilsen <jacksnilsen@gmail.com>
2025-06-23 15:34:31 -0500

    Annotate class, create constructor with Autowired, and refractor

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,8 +1,7 @@
 package org.yearup.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -10,17 +9,20 @@ import org.yearup.models.Product;
 
 import java.util.List;
 
-// add the annotations to make this a REST controller
-// add the annotation to make this controller the endpoint for the following url
-    // http://localhost:8080/categories
-// add annotation to allow cross site origin requests
+// annotations for class
+@RestController
+@RequestMapping("/categories")
+@CrossOrigin
 public class CategoriesController
 {
     private CategoryDao categoryDao;
-    private ProductDao productDao;
-
 
     // create an Autowired controller to inject the categoryDao and ProductDao
+    @Autowired
+    public CategoriesController(CategoryDao categoryDao)
+    {
+        this.categoryDao = categoryDao;
+    }
 
     // add the appropriate annotation for a get action
     public List<Category> getAll()
@@ -36,14 +38,13 @@ public class CategoriesController
         return null;
     }
 
-    // the url to return all products in category 1 would look like this
-    // https://localhost:8080/categories/1/products
-    @GetMapping("{categoryId}/products")
-    public List<Product> getProductsById(@PathVariable int categoryId)
-    {
-        // get a list of product by categoryId
-        return null;
-    }
+    // decided to do this method in the ProductsController instead
+//    @GetMapping("{categoryId}/products")
+//    public List<Product> getProductsById(@PathVariable int categoryId)
+//    {
+//        // get a list of product by categoryId
+//        return null;
+//    }
 
     // add annotation to call this method for a POST action
     // add annotation to ensure that only an ADMIN can call this function
